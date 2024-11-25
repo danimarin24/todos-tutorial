4e308f8482bc437334fc99819401028b3786e546
Daniel Marín <danii04.mb@gmail.com>
2024-11-25 17:37:10 +0100

    feat: step 5 - update and delete todos from supabase

--- app/todos/actions.ts
+++ app/todos/actions.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { getUser } from "@/queries/user";
+import { Todo } from "@/types/custom";
 import { createClient } from "@/utils/supabase/server";
+import exp from "constants";
 import { revalidatePath } from "next/cache";
 
 export const createTodo = async (formData: FormData) => {
@@ -25,6 +27,46 @@ export const createTodo = async (formData: FormData) => {
 
   if (error) {
     throw new Error("Error creating task");
+  }
+
+  revalidatePath("/todos");
+};
+
+export const deleteTodo = async (id: number) => {
+  const supabase = await createClient();
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase.from("todos").delete().match({
+    user_id: user.id,
+    id: id,
+  });
+
+  if (error) {
+    throw new Error("Error deleting task");
+  }
+
+  revalidatePath("/todos");
+};
+
+export const updateTodo = async (todo: Todo) => {
+  const supabase = await createClient();
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase.from("todos").update(todo).match({
+    user_id: user.id,
+    id: todo.id,
+  });
+
+  if (error) {
+    throw new Error("Error updating task");
   }
 
   revalidatePath("/todos");
